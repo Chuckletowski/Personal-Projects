b98ae395c81be77b251070972a5ab90419d68aa5
Chuckletowski <46592355+Chuckletowski@users.noreply.github.com>
2019-11-26 17:07:22 +0800

    Update MortgageCalculator.py

--- MortgageCalculator.py
+++ MortgageCalculator.py
@@ -12,4 +12,4 @@ interestRate = float(input('What is the current annual interest rate? %')) * .01
 numberYears = float(input('How many years do you plan to pay the loan? ')) * 12
 monthlyPayment = (loanAmount * (interestRate * (1 + interestRate) * numberYears)) / ((1 + interestRate) * numberYears - 1)
 
-print('\n\n''Your monthly payment would be: $%.2f' %monthlyPayment)
+print('\n\nYour monthly payment would be: $%.2f' %monthlyPayment)
