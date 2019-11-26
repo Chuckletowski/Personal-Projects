7ea90866cea420a8a1295f2259934645c7b906c5
Chuckletowski <46592355+Chuckletowski@users.noreply.github.com>
2019-11-26 17:19:17 +0800

    Update ProjectDeadlineCalculator.py

--- ProjectDeadlineCalculator.py
+++ ProjectDeadlineCalculator.py
@@ -1,7 +1,5 @@
-#Help determine how much time someone has left to meet a deadline.
-#Ask a user to enter the deadline for their project.
-#Tell them how many days they have left to complete the project.
-#For extra credit, give them the answer as a combination of weeks & days.
+#Help determine how much time someone has left to meet a deadline. Ask a user to enter the deadline for their project.
+#Tell them how many days they have left to complete the project. For extra credit, give them the answer as a combination of weeks & days.
 #(Hint: you will need some of the math functions from the module on numeric values)
 
 import datetime
@@ -12,4 +10,4 @@ currentDate = datetime.date.today()
 remainingWeeks = ((dueDate - currentDate) / 7).days
 remainingDays = int((dueDate - currentDate).days) % 7
 
-print('\n\n'f'There are only {remainingWeeks} week/s and {remainingDays} day/s left before the project is due')
+print(f'\n\nThere are only {remainingWeeks} week/s and {remainingDays} day/s left before the project is due')
