f119d13afcd3e21b95638b8229df96efb3cd2075
Chuckletowski <46592355+Chuckletowski@users.noreply.github.com>
2019-12-05 15:04:04 +0800

    Update 03ProjectDeadlineCalculator.py

--- 03ProjectDeadlineCalculator.py
+++ 03ProjectDeadlineCalculator.py
@@ -1,6 +1,6 @@
 # Help determine how much time someone has left to meet a deadline. Ask a user to enter the deadline for their project.
-# Tell them how many days they have left to complete the project. For extra credit, give them the answer as a combination of weeks & days.
-# (Hint: You will need some of the math functions from the module on numeric values).
+# Tell them how many days they have left to complete the project. For extra credit, give them the answer as a combination
+# of weeks & days. (Hint: You will need some of the math functions from the module on numeric values).
 
 import datetime
 
