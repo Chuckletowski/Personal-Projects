cc0b3b51cc730817b18dd2e4d3caea313b1feff8
Chuckletowski <46592355+Chuckletowski@users.noreply.github.com>
2019-12-12 17:13:39 +0800

    Update 06_pyramid_drawing.py

--- 06_pyramid_drawing.py
+++ 06_pyramid_drawing.py
@@ -1,4 +1,5 @@
-# Write a program that will draw a pyramid figure on the screen. Ask the user how tall they want their pyramid to be. For extra credit, let them choose what character on the keyboard they want to use for their drawing.
+# Write a program that will draw a pyramid figure on the screen. Ask the user how tall they want their pyramid to be.
+# For extra credit, let them choose what character on the keyboard they want to use for their drawing.
 
 character = input('What character do you want to use for drawing? ')
 row = int(input('How many rows do you want your pyramid to have? '))
