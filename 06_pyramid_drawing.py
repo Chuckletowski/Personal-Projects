19472537f803bbcfe8b8c84c89312e1f2d901b74
Chuckletowski <46592355+Chuckletowski@users.noreply.github.com>
2019-12-12 17:10:45 +0800

    Add files via upload

---
+++ 06_pyramid_drawing.py
@@ -0,0 +1,15 @@
+# Write a program that will draw a pyramid figure on the screen. Ask the user how tall they want their pyramid to be. For extra credit, let them choose what character on the keyboard they want to use for their drawing.
+
+character = input('What character do you want to use for drawing? ')
+row = int(input('How many rows do you want your pyramid to have? '))
+space = row - 1
+draw = 1
+
+for r in range(row):
+    for s in range(space):
+        print(' ', end = '')
+    space -= 1
+    for d in range(draw):
+        print(character, end = '')
+    draw += 2
+    print()
